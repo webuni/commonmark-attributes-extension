7ce9e1396b29166cfa333cdd875136496112d5c5
Martin Hasoň <martin.hason@gmail.com>
2015-07-13 15:18:28 +0200

    Simplified arguments in AttributesUtils::merge method

--- src/AttributesUtils.php
+++ src/AttributesUtils.php
@@ -86,10 +86,10 @@ class AttributesUtils
         return $attributes;
     }
 
-    public static function merge(/*$array1, $array2 = []*/)
+    public static function merge($attributes1, $attributes2)
     {
         $attributes = [];
-        foreach (func_get_args() as $arg) {
+        foreach ([$attributes1, $attributes2] as $arg) {
             if (($arg instanceof AbstractBlock || $arg instanceof AbstractInline)) {
                 $arg = isset($arg->data['attributes']) ? $arg->data['attributes'] : [];
             }
